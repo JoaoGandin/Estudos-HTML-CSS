f4a6404f0944745cb4c4737558d277eb1f9910e5
JoaoGandin <joaovictorgoncalvesgandin@gmail.com>
2026-04-04 11:48:15 -0300

    add ex008.js and ex009.js: implement speed check and nationality identification examples in JavaScript

---
+++ javascript/aula011/ex008.js
@@ -0,0 +1,8 @@
+// execuntando com o node.js
+// no terminal coloca node <nome do arquivo com extensão js>
+var vel = 60.5;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if(vel > 60){ //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando sinto de segurança!`);
