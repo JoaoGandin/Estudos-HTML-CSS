dd9bee07877e6790951516e53b0058d550aac2f3
JoaoGandin <joaovictorgoncalvesgandin@gmail.com>
2026-04-05 09:59:12 -0300

    add objeto02.js: define carro object with properties and comments in JavaScript

---
+++ javascript/aula017/objeto02.js
@@ -0,0 +1,5 @@
+// propriedade é o conjunto do objeto (key + valor, ex: marca:"ford")
+// key é o nome para identificar (marca, ano, modelo...)
+// valor é o dado que ela guarda.
+// se num objeto tiver uma "função" chamamos ela de método.
+let carro = {marca:"ford", modelo: "ka", ano:2015, placa:"ABC-1234"};
