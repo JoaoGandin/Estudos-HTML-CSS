405dd1f77966ba3a33b4700fade8a26b5e89da48
JoaoGandin <joaovictorgoncalvesgandin@gmail.com>
2026-04-05 10:17:27 -0300

    refactor objeto02.js: enhance object structure and add comments for clarity add objetos.md: document object types and usage in JavaScript

--- javascript/aula017/objeto02.js
+++ javascript/aula017/objeto02.js
@@ -1,5 +1,14 @@
 // propriedade é o conjunto do objeto (key + valor, ex: marca:"ford")
 // key é o nome para identificar (marca, ano, modelo...)
 // valor é o dado que ela guarda.
-// se num objeto tiver uma "função" chamamos ela de método.
-const carro = {marca:"ford", modelo: "ka", ano:2015, placa:"ABC-1234"};
+// se num objeto tiver uma "função" chamamos ela de método
+// é uma prática, no JavaScript, escrever objetos utilizando o const
+
+// OBJETO LITERAL: determinamos as propriedades que ele vai ter
+const carro = {
+    marca:"ford",
+    modelo: "ka",
+    ano:2015,
+    placa:"ABC-1234",
+    buzina: function() {alert("Biiiiiiiiiii")}
+};
---
+++ javascript/aula017/objetos.md
@@ -0,0 +1,8 @@
+# Objetos
+
+## Objetos Literais
+Objetos literais é a forma comum. É quando você já declara as propriedades e os valores dele.
+**Uso:** Ideal para dados rápidos, configurações ou quando você só precisa de uma instância única daquele objeto.
+
+## Objetos Não Literais (Instanciados)
+São objetos criados a partir de um "molde" (uma Classe ou uma Função Construtora) usando a palavra-chave ```javascript new```.
